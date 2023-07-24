97cb0f69aa82089a33e0bc23252c0e878bf959e4
sebsgamez13 <139572213+sebsgamez13@users.noreply.github.com>
2023-07-24 17:57:10 -0500

    Add files via upload

---
+++ JS/Main.js
@@ -0,0 +1,11 @@
+const suma=()=>{
+    let a=0;
+    let b=0;
+    let result;
+
+    a = parseInt(prompt ("Por Favor ingrese el primer valor"));
+    b = parseInt(prompt("Por favor ingrese el segundo valor"));
+
+    result = a +b
+    alert("la suma es igual a:" + suma)
+}
